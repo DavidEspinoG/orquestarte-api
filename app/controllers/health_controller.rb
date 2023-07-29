c964bc2c2813aa0ec4d47d5f3a61695cd1bcbdc8
David Espino <davidespinog@gmail.com>
2023-07-29 10:17:57 -0600

    Add Excon gem

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -2,4 +2,9 @@ class HealthController < ApplicationController
   def index
     render json: {message: 'ok'}, status: :ok
   end
+
+  def fox 
+    response = Excon.get('https://randomfox.ca/floof/');
+    render json: response.body, status: :ok
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/health', to: 'health#index'
+  get '/fox', to: 'health#fox'
   # Defines the root path route ("/")
   # root "articles#index"
 end
