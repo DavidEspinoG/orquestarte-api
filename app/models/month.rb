b3f1f3ae7ed5cf2041a58b86ab3e4f7000734c5b
David Espino <davidespinog@gmail.com>
2023-08-16 15:39:03 -0600

    Add callback to student model

--- app/models/month.rb
+++ app/models/month.rb
@@ -1,4 +1,5 @@
 class Month < ApplicationRecord
   belongs_to :student
   belongs_to :school_year
+  validates :name, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,26 @@ class Student < ApplicationRecord
   belongs_to :user
   belongs_to :school
   has_many :months
+
+  after_create :create_months
+
+  private 
+
+  def create_months
+    school_year = SchoolYear.first
+    months = Month.create([
+      {name: 'Septiembre', student: self, school_year: school_year},
+      {name: 'Octubre', student: self, school_year: school_year},
+      {name: 'Noviembre', student: self, school_year: school_year},
+      {name: 'Diciembre', student: self, school_year: school_year},
+      {name: 'Enero', student: self, school_year: school_year},
+      {name: 'Febrero', student: self, school_year: school_year},
+      {name: 'Marzo', student: self, school_year: school_year},
+      {name: 'Abril', student: self, school_year: school_year},
+      {name: 'Mayo', student: self, school_year: school_year},
+      {name: 'Junio', student: self, school_year: school_year},
+      {name: 'Julio', student: self, school_year: school_year},
+    ])
+  end
+
 end
