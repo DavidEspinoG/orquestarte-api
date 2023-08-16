2531b75f080bea0f2607cb2d94bbad3cb79d7fcb
David Espino <davidespinog@gmail.com>
2023-08-15 19:52:45 -0600

    Add user model

---
+++ db/migrate/20230816013115_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :phone_number
+      t.boolean :is_admin, default: false, null: false
+
+      t.timestamps
+    end
+  end
+end
