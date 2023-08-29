f2acd36449207ae70404db8986d91f4f0ec24184
David Espino <davidespinog@gmail.com>
2023-08-29 12:44:23 -0600

    Getting ready to deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,21 @@
 #   Character.create(name: "Luke", movie: movies.first)
 school = School.create(name: 'Walter C. Buchanan', code: 'BUCH2023')
 
-users = User.create(
-  [
-    {first_name: 'David',last_name: 'Espino', email: 'david@mail.com', password: 'password', is_admin: true, school: school},
-    {first_name: 'Elena',last_name: 'Garcia', email: 'elena@mail.com', password: 'password', is_admin: true, school: school},
-    {first_name: 'José',last_name: 'Hernandez', email: 'jose@mail.com', password: 'password', school: school}
-  ]
-)
+# users = User.create(
+#   [
+#     {first_name: 'David',last_name: 'Espino', email: 'david@mail.com', password: 'password', is_admin: true, school: school},
+#     {first_name: 'Elena',last_name: 'Garcia', email: 'elena@mail.com', password: 'password', is_admin: true, school: school},
+#     {first_name: 'José',last_name: 'Hernandez', email: 'jose@mail.com', password: 'password', school: school}
+#   ]
+# )
 
 school_year = SchoolYear.create(name: '2023-2024')
 
-student_one = Student.create(
-  first_name: 'Jose',
-  last_name: 'Junior',
-  active: true, 
-  user: User.third, 
-  school: school
-)
+# student_one = Student.create(
+#   first_name: 'Jose',
+#   last_name: 'Junior',
+#   active: true, 
+#   user: User.third, 
+#   school: school
+# )
 
